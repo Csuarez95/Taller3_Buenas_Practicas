88743e3b1af1242f538584f4411ef09bcac84de6
yvahosc <yvahosc@unal.edu.co>
2022-04-18 23:37:01 -0500

    Validación de fecha y texto
    Se añade en la clase Input los métodos para validar que la fecha de la canción sea ingresada en el formato yyyy-mm-dd y que los textos ingresados por los usuarios por medio del teclado no esten vacios, empleando el try-catch para verificar los valores ingresados mostrando un mensaje de advertencia.

--- Taller3_BuenasPracticas/src/com/company/Classes/Input.java
+++ Taller3_BuenasPracticas/src/com/company/Classes/Input.java
@@ -1,8 +1,53 @@
 package com.company.Classes;
 
+import java.time.LocalDate;
 import java.util.Scanner;
 
+/**
+ * Claro para la validación de los datos que son ingresados por teclado.
+ * @author Cristian Suárez Acosta
+ * @author Yeisson Vahos Cortes
+ */
 public class Input {
+
+    /**
+     * Método para validación que no haya un campo vacío en la entrada de
+     * texto.
+     * @param string Parámetro para almacenar el texto ingresado por teclado.
+     * @return Texto ingresado.
+     */
+    public static String verificationString (String string) {
+        while (string.isEmpty()){
+            Scanner input = new Scanner(System.in);
+            System.out.println("El campo de texto no puede estar vacío, por " +
+                    "favor ingrese el texto soliciado");
+            string = input.nextLine();
+        }
+        return string;
+    } //Cierre del método verificationString.
+
+    /**
+     * Método para validación del ingreso de la fecha en el formato
+     * correspondiente.
+     * @return Fecha de la canción.
+     */
+    public static LocalDate verificationDate () {
+        Boolean verification = false;
+        Scanner input = new Scanner(System.in);
+        LocalDate date = null;
+        while (!verification){
+            try {
+                System.out.println("Ingrese la fecha de la canción en formato" +
+                        " YYYY-MM-DD");
+                date = LocalDate.parse(input.nextLine());
+                verification = true;
+            } catch (Exception e) {
+                System.out.println("La fecha debe ser ingresada en formato " +
+                        "yyyy-mm-dd");
+            }
+        } //Cierre del ciclo While.
+        return date;
+    } //Cierre del método verificationDate.
 
     /**
      * Método para validación del ingreso de la duración en segundos.
