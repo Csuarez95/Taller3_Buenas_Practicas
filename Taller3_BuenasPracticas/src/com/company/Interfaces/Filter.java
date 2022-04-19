1b2ea9ed443a4ee037d3450620686096e5e2aaa0
yvahosc <yvahosc@unal.edu.co>
2022-04-19 11:28:43 -0500

    Creación del método filtrar
    Creación del método para filtrar las canciones de la biblioteca de canciones por su año de lanzamiento.

--- Taller3_BuenasPracticas/src/com/company/Interfaces/Filter.java
+++ Taller3_BuenasPracticas/src/com/company/Interfaces/Filter.java
@@ -1,4 +1,37 @@
 package com.company.Interfaces;
 
+import com.company.Classes.Song;
+import com.company.Classes.SongList;
+
+import java.util.ArrayList;
+
+/**
+ * Interfaz creada para el filtrado de las canciones por género y año.
+ * @author Cristian Suárez Acosta
+ * @author Yeisson Vahos Cortes
+ */
 public interface Filter {
+
+    /**
+     * Método para filtrado del año heredado de la interfaz Filter.
+     * @param songs Objeto con las características de la canción.
+     * @param year Parámetro que almacena el género de la canción.
+     */
+    static void yearFilter(ArrayList<SongList> songs, int year) {
+        ArrayList<SongList> listFilterByYear = new ArrayList<SongList>();
+        for (SongList i: songs) {
+            if (i.getDate().getYear() == year){
+                listFilterByYear.add(i);
+            }
+        }
+        if (listFilterByYear.isEmpty()){
+            System.out.println("No existen canciones asociadas al año " +
+                    "ingresado.");
+        } else {
+            for (SongList j: listFilterByYear){
+                System.out.println(j.toString());
+            }
+        }
+
+    } //Cierre del método yearFilter.
 }
