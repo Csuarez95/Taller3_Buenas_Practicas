49ab2f231f296ea68254d15875a6a14d35830076
yvahosc <yvahosc@unal.edu.co>
2022-04-20 13:18:20 -0500

    Actualización clase Main

--- Taller3_BuenasPracticas/src/com/company/Main/Main.java
+++ Taller3_BuenasPracticas/src/com/company/Main/Main.java
@@ -1,5 +1,13 @@
 package com.company.Main;
 
+import com.company.Classes.Input;
+import com.company.Classes.Song;
+import com.company.Classes.SongList;
+import com.company.Interfaces.Filter;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Programa para organizar la bibliotecla de música.
  * @author Cristian Suárez Acosta.
@@ -52,6 +60,209 @@ public class Main {
                     SongList.addSong(mainList);
                     break;
                 case 2:
+                    boolean flag2 = true;
+                    while (flag2){
+                        if (mainList.isEmpty()) {
+                            System.out.println("La biblioteca de canciones se" +
+                                    " encuentra vacía\n");
+                            flag2 = false;
+                        } else {
+                            System.out.println("Lista de biblioteca");
+                            for (Song i: mainList) {
+                                System.out.println(i.toString());
+                            }
+                            System.out.println("Seleccione una de las " +
+                                    "siguientes opciones");
+                            System.out.println("1. Crear Playlist \n2. " +
+                                    "Eliminar canción \n3. Filtrar canciones " +
+                                    "\n4. Organizar canciones \n5. Salir");
+                            int musicMenu = Input.verificationNumber();
+
+                            switch (musicMenu) {
+                                case 1:
+                                    if (playList.isEmpty()) {
+                                        playList.add(SongList.createPlayList(
+                                                mainList, playList));
+                                        System.out.println("Lista actual de " +
+                                                "Playlist");
+                                        for (Song i: playList) {
+                                            System.out.println(i.toString());
+                                        }
+                                        boolean flag3 = true;
+                                        while (flag3){
+                                            System.out.println("¿Desea " +
+                                                    "agregar otra canción?");
+                                            System.out.println("1. SI\n 2. NO");
+                                            int option =
+                                                    Input.verificationNumber();
+                                            switch (option) {
+                                                case 1:
+                                                    SongList songForPlayList =
+                                                        SongList.createPlayList(
+                                                                mainList,
+                                                                playList);
+                                                    if (songForPlayList != null)
+                                                    {
+                                                        playList.add(
+                                                                songForPlayList);
+                                                        System.out.println(
+                                                            "Lista actual" +
+                                                                    " de " +
+                                                                    "Playlist");
+                                                        for (Song i: playList) {
+                                                            System.out.println(
+                                                                i.toString());
+                                                        }
+                                                    }
+                                                    break;
+                                                case 2:
+                                                    System.out.println("Lista" +
+                                                            " creada de Playlist");
+                                                    for (Song i: playList) {
+                                                        System.out.println(
+                                                                i.toString());
+                                                    }
+                                                    flag3 = false;
+                                                    break;
+                                                default:
+                                                    System.out.println(
+                                                            "Ingrese opción " +
+                                                                    "válida");
+                                            }
+                                        } //Cierre while flag3.
+                                    } else {
+                                        boolean flag4 = true;
+                                        while (flag4){
+                                            System.out.println("¿Desea " +
+                                                    "reemplazar la Playlist " +
+                                                    "existente?");
+                                            System.out.println("1. SI \n2. NO");
+                                            int option1 =
+                                                    Input.verificationNumber();
+                                            switch (option1) {
+                                                case 1:
+                                                    playList.removeAll(playList);
+                                                    playList.add(
+                                                            SongList.createPlayList(
+                                                                    mainList,
+                                                                    playList));
+                                                    System.out.println("Canci" +
+                                                            "ón agregada a la" +
+                                                            " playlist");
+                                                    boolean flag3 = true;
+                                                    while (flag3){
+                                                        System.out.println(
+                                                            "¿Desea agregar " +
+                                                                "otra canción?");
+                                                        System.out.println("1" +
+                                                                ". SI \n2. NO");
+                                                        int option2 =
+                                                            Input.
+                                                            verificationNumber();
+                                                        switch (option2) {
+                                                            case 1:
+                                                                playList.add(
+                                                                    SongList.
+                                                                    createPlayList(
+                                                                        mainList,
+                                                                        playList));
+                                                                System.out.println(
+                                                                    "Canción " +
+                                                                    "agregada" +
+                                                                    " a la " +
+                                                                    "playlist");
+                                                                break;
+                                                            case 2:
+                                                                System.out.println(
+                                                                    "Lista " +
+                                                                    "creada " +
+                                                                    "de " +
+                                                                    "Playlist");
+                                                                for (Song i:
+                                                                        playList)
+                                                                {
+                                                                    System.out.
+                                                                    println(
+                                                                    i.toString());
+                                                                }
+                                                                flag3 = false;
+                                                                flag4 = false;
+                                                                break;
+                                                            default:
+                                                                System.out.println(
+                                                                    "Ingrese " +
+                                                                    "opción " +
+                                                                    "válida");
+                                                        }
+                                                    }
+                                                    break;
+                                                case 2:
+                                                    flag4 = false;
+                                                    break;
+                                                default:
+                                                    System.out.println("" +
+                                                        "Ingrese opción válida");
+                                            }
+                                        } //Cierre de while con flag4.
+                                    }
+                                    break;
+                                case 2:
+
+                                    break;
+                                case 3:
+                                    boolean flag6 = true;
+                                    while (flag6) {
+                                        System.out.println("Filtrado de " +
+                                            "canciones de la biblioteca");
+                                        System.out.println("¿Que tipo de " +
+                                            "filtro desea aplicar a la " +
+                                                "biblioteca?");
+                                        System.out.println("1. Por género \n2" +
+                                            ". Por año\n3. Regresar al menú");
+                                        int option2 = Input.verificationNumber();
+                                        switch (option2){
+                                            case 1:
+                                                System.out.println("¿Por cual" +
+                                                        " género desea filtrar?");
+                                                Scanner input =
+                                                        new Scanner(System.in);
+                                                String genre = input.nextLine();
+                                                genre =
+                                                        Input.verificationString(
+                                                                genre);
+                                                Filter.genreFilter(mainList,
+                                                        genre);
+                                                break;
+                                            case 2:
+                                                System.out.println("¿Por cual" +
+                                                        " año desea filtrar?");
+                                                int year =
+                                                        Input.verificationNumber();
+                                                Filter.yearFilter(mainList,
+                                                        year);
+                                                break;
+                                            case 3:
+                                                flag6 = false;
+                                                break;
+                                            default:
+                                                System.out.println("Ingrese " +
+                                                        "opción válida");
+                                        }
+                                    }
+                                    break;
+                                case 4:
+
+                                    break;
+                                case 5:
+                                    System.out.println("Saliendo al menú " +
+                                            "inicial");
+                                    flag2 = false;
+                                    break;
+                                default:
+
+                            } //Cierre del condicional musicMenu
+                        }
+                    } //Cierre while flag2
                     break;
                 case 3:
                     System.out.println("Saliendo del programa");
@@ -63,6 +274,5 @@ public class Main {
                             "opciones dadas");
             }
         }
-
     }
 }
